0a14f3247d8b54d448fe39333de3f5170503218a
Prince <prince00goswami@gmail.com>
2026-01-09 19:28:18 +0530

    program of fucntion and async functions and object and this operator , call /bind/apply , closure , throttle etc , These are the interview relevant questions

--- practise/js programs/gpt_suggestedprogram.js
+++ practise/js programs/gpt_suggestedprogram.js
@@ -28,3 +28,75 @@ Math.min(...arr)
 
 arr.filter((v,i,a)=>a.indexOf(v)!==i)
 
+// Closure example
+
+function outer(){
+  let x=10
+  return ()=>x++
+}
+
+
+// Currying
+
+const add=a=>b=>a+b
+
+
+// Debounce
+
+function debounce(fn,delay){
+ let t
+ return (...a)=>{
+  clearTimeout(t)
+  t=setTimeout(()=>fn(...a),delay)
+ }
+}
+
+
+// Throttle
+
+function throttle(fn,delay){
+ let flag=true
+ return (...a)=>{
+  if(flag){
+   fn(...a)
+   flag=false
+   setTimeout(()=>flag=true,delay)
+  }
+ }
+}
+
+// ⏳ ASYNC (VERY IMPORTANT)
+
+// Promise example
+
+new Promise((res,rej)=>res("done"))
+
+
+Async/Await
+
+async function f(){
+ const r=await fetch(url)
+}
+
+
+// Promise.all
+
+Promise.all([p1,p2,p3])
+
+// 🧩 OBJECT & THIS
+
+// Deep copy object
+
+JSON.parse(JSON.stringify(obj))
+
+
+// this keyword example
+
+obj.method.call(otherObj)
+
+
+// Bind / Call / Apply
+
+fn.call(obj,a,b)
+fn.apply(obj,[a,b])
+fn.bind(obj)
