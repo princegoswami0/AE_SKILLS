3f42dc85b854d16b1f0382cefca1e8bff5284aa1
Prince <prince00goswami@gmail.com>
2026-01-11 00:57:31 +0530

    chore(frontend): implement role-based routing for admin and user flows

--- mega-pro/task-manager/frontend/task-manager/src/App.jsx
+++ mega-pro/task-manager/frontend/task-manager/src/App.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//auth pages
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+
+//admin pages
+import Dashboard from './pages/Admin/Dashboard';
+import PrivateRoute from './routes/PrivateRoute';
+import ManageTasks from './pages/Admin/ManageTasks';
+import CreateTask from './pages/Admin/CreateTask';
+import ManageUsers from './pages/Admin/ManageUsers';
+
+//user pages
+import UserDashboard from './pages/User/UserDashboard';
+import MyTasks from './pages/User/MyTasks';
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+
 
 const App = () => {
   return (
-    <div>  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes  */}
+
+          <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+          </Route>
+
+            {/* User Routes  */}
+          <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/my-tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
