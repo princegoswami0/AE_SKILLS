0511925298946f80ad4cdec5a482d7439add0676
Prince <prince00goswami@gmail.com>
2026-01-11 12:12:21 +0530

    feat(models): add User and Task schemas

---
+++ mega-pro/task-manager/backend/models/Task.js
@@ -0,0 +1,28 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+const { create } = require('./User');
+
+const todoSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  }
+);
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String},
+    priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Medium' },
+    status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
+    dueDate: { type: Date, required: true },
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    attachments: [{ type: String }], // Array of file URLs
+    todoChecklist: [todoSchema],
+    progress: {type: Number, default: 0}, // Progress percentage
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Task', taskSchema);
