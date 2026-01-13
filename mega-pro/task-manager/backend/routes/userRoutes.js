9b552537b551d491da42eef84a0559236c382f9f
Prince <prince00goswami@gmail.com>
2026-01-13 22:56:57 +0530

    create user route in the task_manager_app

--- mega-pro/task-manager/backend/routes/userRoutes.js
+++ mega-pro/task-manager/backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ const { adminOnly, protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 //User Routes
-router.get("/",protect,adminOnly,getUsers);// Get all users (admin only)
+router.get("/",protect,adminOnly,getUsers);// Get all users (admin only) 
+router.get("/:id",protect, getUserById);// Get user by ID
+router.delete("/:id", protect, adminOnly, deleteUser); // Delete user (admin only)
 
-
+module.exports = router;
