2184e0730b092ef6ff3da6082dba0f156de0c7fc
Prince <prince00goswami@gmail.com>
2026-01-30 13:50:04 +0530

    leraning dsa

--- Dsa/conditional_statements/check-prime.cpp
+++ Dsa/conditional_statements/check-prime.cpp
@@ -2,24 +2,37 @@
 using namespace std;
 
 int main(){
-  int n;
-  cout<<"enter the no =";
-  cin>>n;
-  bool isPrime = true;
+  // int n;
+  // cout<<"enter the no =";
+  // cin>>n;
+  // bool isPrime = true;
 
 
-  for (int i = 2; i*i<=n ; i++){
-    if (n%i==0){
-      isPrime = false;
-      break;
-    }
+  // for (int i = 2; i*i<=n ; i++){
+  //   if (n%i==0){
+  //     isPrime = false;
+  //     break;
+  //   }
+  // }
+
+  // if (isPrime){
+  //   cout << n << " is prime no"<< endl;
+  // }else{
+  //   cout<< n << "is not primeno"<< endl; 
+  // }
+// int n =10;
+//   for (int i = 1; i<=n;i++){
+//     cout<< "*****\n";
+//   }
+
+int n = 2;
+for (int i= 1; i<=n; i++){
+  int m =2;
+  for(int j = 1; j<=m; j++){
+    cout<<"*\n";
   }
-
-  if (isPrime){
-    cout << n << " is prime no"<< endl;
-  }else{
-    cout<< n << "is not primeno"<< endl; 
-  }
+  
+}
 
   return 0;
 }
