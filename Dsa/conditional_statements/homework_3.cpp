762f31e000e27de24da49f7c08fdd55406474f6a
Prince <prince00goswami@gmail.com>
2026-01-30 23:42:47 +0530

    writing other logic for the program of the Floyd;s triangle .

--- Dsa/conditional_statements/homework_3.cpp
+++ Dsa/conditional_statements/homework_3.cpp
@@ -167,9 +167,36 @@ int main(){
 	// 3 2 1
 	// 4 3 2 1 
 
-	
+	// int n =4;
+
+	// for (int i =0; i<n ; i++){
+	// 	for (int j = i+1; j>0 ; j--){
+	// 		cout << j ;
+
+	// 	}
+	// 	cout<<endl;
+	// }
 
 
-  return 0;
+
+	///////////////////////////////
+
+	// 1
+	// 2 3
+	// 4 5 6 
+	// 7 8 9 10
+
+
+	int n =4;
+	int num =1;
+
+	for(int i =0; i<n ;i++){
+		for(int j=0; j<=i; j++){
+			cout<< num;
+			num++;
+		}
+		cout<<endl;
+	}
+  return 0; 
 }
 
