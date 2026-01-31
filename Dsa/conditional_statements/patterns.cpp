16a43878d194f71e0f0077c2ca30e3283d181a7e
Prince <prince00goswami@gmail.com>
2026-01-31 13:57:05 +0530

    practise problem for the dsa pattern problems " _ _ _ 1 _ _ 1 2 1 _ 1 2 3 2 1 1 2 3 4 3 2 1"
    using the c++

--- Dsa/conditional_statements/patterns.cpp
+++ Dsa/conditional_statements/patterns.cpp
@@ -2,21 +2,50 @@
 using namespace std;
 
 int main (){
-  int n =4;
-  char ch= 'A';
+  // int n =4;
+  // char ch= 'A';
 
-  for(int i = 0; i<n ; i++){
-    for(int j = 0; j <i ; j++){
+  // for(int i = 0; i<n ; i++){
+  //   for(int j = 0; j <i ; j++){
+  //     cout << " ";
+  //   }
+
+  //   for(int j =0; j<n-i; j++){
+  //   cout << char('A' + i);
+  //   }
+  //   // ch++;
+  //   cout<<endl;
+  // }
+
+  // PYRAMID PATTERN /////////////
+
+
+  int n =4;
+  
+  for (int i =0 ; i<n; i++){
+    //spaces
+
+    for (int j = 0 ; j < n-i-1 ; j++){
       cout << " ";
     }
 
-    for(int j =0; j<n-i; j++){
-    cout << char('A' + i);
+    // increasing nuumbers 
+
+    for ( int j = 0 ; j < i+1 ; j++){
+      cout << (j+1);
     }
-    // ch++;
+
+    //decreasing
+
+    for(int j = i ; j>=1 ; j--){
+      cout<< j;
+    }
     cout<<endl;
   }
   return 0 ;
 }
 
 
+
+
+ 
